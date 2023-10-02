f0a2b15de1db902f3e9cdac81fe1839c735552a3
vivek <vivek@gmail.com>
2023-10-02 11:26:10 +0530

    Bsic More changes

--- app/src/main/java/com/example/food_order/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/food_order/Fragment/HistoryFragment.kt
@@ -1,60 +1,75 @@
 package com.example.food_order.Fragment
 
+import CartBindingAdapter
+import PopularBindingAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.food_order.DataModel.YourDataModel
+import com.example.food_order.DataModel.cartdata
 import com.example.food_order.R
+import com.example.food_order.adapter.MenuAdapter
+import com.example.food_order.databinding.FragmentCartBinding
+import com.example.food_order.databinding.FragmentHistoryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentHistoryBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        return  binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        RsetupRecyclerView()
+        PsetupRecyclerView()
     }
+    private fun RsetupRecyclerView() {
+        val recyclerView = binding.recentlyBoughtRecyclerView
+        val dataSet = listOf(
+            YourDataModel(R.drawable.menu1, "Middle Text 1", "5"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+
+            // Add more data items as needed
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val adapter = PopularBindingAdapter(dataSet)
+        recyclerView.adapter = adapter
+    }
+    private fun PsetupRecyclerView() {
+        val recyclerView = binding.previouslyBoughtRecyclerView
+        val dataSet = listOf(
+            YourDataModel(R.drawable.menu1, "Middle Text 1", "5"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6")
+            // Add more data items as needed
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val adapter = PopularBindingAdapter(dataSet)
+        recyclerView.adapter = adapter
+    }
+
+
+
 }
--- app/src/main/java/com/example/food_order/Fragment/SearchFragment.kt
+++ app/src/main/java/com/example/food_order/Fragment/SearchFragment.kt
@@ -5,29 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.food_order.DataModel.YourDataModel
 import com.example.food_order.R
+import com.example.food_order.adapter.MenuAdapter
+import com.example.food_order.databinding.FragmentBottomsheetfragmentBinding
+import com.example.food_order.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +25,33 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater,container,false)
+        setupRecyclerView()
+
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    private fun setupRecyclerView() {
+        val recyclerView = binding.searchrecycler
+        val dataset1 = listOf(
+            YourDataModel(R.drawable.menu1, "Middle Text 1", "5"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6"),
+            YourDataModel(R.drawable.menu2, "Burger", "6")
+            // Add more data items as needed
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val adapter = MenuAdapter(dataset1)
+        recyclerView.adapter = adapter
     }
+
+
 }
